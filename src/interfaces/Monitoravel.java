3403b002a56d0d03fd7c58eccdbc87a9718a284e
matheus <matheusgurther@outlook.com>
2026-05-28 19:56:21 -0300

    criando interfaces Monitoravel e Notificavel

---
+++ src/interfaces/Monitoravel.java
@@ -0,0 +1,8 @@
+package interfaces;
+
+public interface Monitoravel {
+
+    void emitirAlerta();
+
+    void gerarRelatório();
+}
---
+++ src/interfaces/Notificavel.java
@@ -0,0 +1,6 @@
+package interfaces;
+
+public interface Notificavel {
+
+    void enviarNotificacao();
+}
